585e310f0bd100b602b8da7972a8a4021a3106de
roshank07 <roshann.purbey@gmail.com>
2024-01-26 19:16:11 +0545

    Enabled Dark mode

--- client/src/Component/Header.jsx
+++ client/src/Component/Header.jsx
@@ -2,13 +2,16 @@ import { Navbar,TextInput,Button, Dropdown, Avatar } from 'flowbite-react';
 import React from 'react';
 import {Link,useLocation} from 'react-router-dom';
 import {AiOutlineSearch} from 'react-icons/ai'
-import {FaMoon} from 'react-icons/fa'
-import {useSelector } from 'react-redux';
+import {FaMoon,FaSun} from 'react-icons/fa'
+import {useSelector,useDispatch} from 'react-redux';
+import { toggleTheme } from '../redux/theme/themeSlice';
 
 
 export default function Header() {
     const path=useLocation().pathname;
-
+    
+    const dispatch=useDispatch();
+    const {theme}=useSelector(state=>state.theme);
     const {currentUser}=useSelector(state=>state.user);
   return (
     <Navbar className='border-b-2'>
@@ -31,8 +34,9 @@ export default function Header() {
             <AiOutlineSearch/>
         </Button>
         <div className='flex gap-2 md:order-2'>
-            <Button className='w-12 h-10 hidden sm:inline' color='grey' pill>
-                <FaMoon />
+            <Button className='w-12 h-10 hidden sm:inline' color='grey' pill 
+            onClick={()=>dispatch(toggleTheme())}>
+                {theme==='light'?(<FaMoon/>):(<FaSun/>)}
             </Button>
             {currentUser?(
                 <Dropdown 
@@ -45,7 +49,6 @@ export default function Header() {
                   rounded
                  />
                  } 
-
                 >
                     <Dropdown.Header>
                         <span className='block text-sm'>@{currentUser.username}</span>
