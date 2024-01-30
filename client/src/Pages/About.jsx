4d8ecc3973ee05b7dd051041c8bc9d7cfc420690
roshank07 <roshann.purbey@gmail.com>
2024-01-30 23:44:54 +0545

    Added Project Page

--- client/src/Pages/About.jsx
+++ client/src/Pages/About.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 function About() {
   return (
-    <div>
-      <p>About</p>
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div className=''>
+          <h1 className='text-3xl font-semibold text-center my-7'>About Roshan's blog </h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6'>
+            Welcome to Roshan's blog. The Purpose of this website is to share the knowledge of Singlestore that i gained
+            from my last professional Experience. The main idea is to pass singlestore knowledge to as many people as possible.
+            I know that we don't have enough external documents/blogs about Singlestore as this is new database 
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- client/src/Pages/Project.jsx
+++ client/src/Pages/Project.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import CallToAction from '../Component/CallToAction';
 
-function Project() {
+export default function Projects() {
   return (
-    <div>
-      <p>Projects</p>
+    <div className='min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3'>
+      <h1 className='text-3xl font-semibold'>Pojects</h1>
+      <p className='text-md text-gray-500'>You can Find more projects on my github</p>
+      <CallToAction />
     </div>
   )
 }
-
-export default Project
