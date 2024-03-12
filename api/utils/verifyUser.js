fdfea5c1c614aaea40c252305c45aa2bf77c0e84
roshank07 <roshann.purbey@gmail.com>
2024-03-12 09:32:01 +0545

    Fixed cookie verification issue in the backend

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -1,24 +1,19 @@
-import jwt from 'jsonwebtoken';
-import errorHandler from './error.js';
+import jwt from "jsonwebtoken";
+import errorHandler from "./error.js";
 
-
-export const verifyUser=async(req,res,next)=>{
-    // console.log('inhere');
-    const token=req.cookies.access_token;
-    if(!token){
-        // console.log('inhere2');
-        return errorHandler(401,'Unauthorized');
-
+export const verifyUser = async (req, res, next) => {
+  // console.log('inhere');
+  const token = req.cookies.access_token;
+  if (!token) {
+    // console.log('inhere2');
+    return next(errorHandler(401, "Unauthorized"));
+  }
+  jwt.verify(token, process.env.JWT_SECRET, (error, user) => {
+    if (error) {
+      return next(errorHandler(401, "Unauthorized"));
+    } else {
+      req.user = user;
+      next();
     }
-    jwt.verify(token,process.env.JWT_SECRET,(error,user)=>{
-        if(error){
-            return errorHandler(401,'Unauthorized');
-        }
-        else{
-            req.user=user;
-            next();
-        }
-    })
-
-
-}
+  });
+};
